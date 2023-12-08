3bb0e72475b80464bc5452da9c546c4a407c4a22
大梦初醒总在一瞬之间 <Halo779500@163.com>
2023-12-08 11:38:39 +0800

    完成项目的初始化工作与搭建

---
+++ openChat-server/src/main/java/com/liaocyu/OpenChatServerApp.java
@@ -0,0 +1,16 @@
+package com.liaocyu;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : create by lcy
+ * @Project : openChat
+ * @createTime : 2023/12/8 11:37
+ * @description :
+ */
+@SpringBootApplication
+public class OpenChatServerApp {
+    public static void main(String[] args) {
+
+    }
+}
