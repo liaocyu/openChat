ce89618a9508b68233118553e3f2b66fc85f29e7
大梦初醒总在一瞬之间 <Halo779500@163.com>
2024-01-12 11:15:06 +0800

    项目集成 Minio && RocketMQ

--- openChat-server/src/main/java/com/liaocyu/openChat/common/chat/consumer/MsgSendConsumer.java
+++ openChat-server/src/main/java/com/liaocyu/openChat/common/chat/consumer/MsgSendConsumer.java
@@ -1,7 +1,9 @@
 package com.liaocyu.openChat.common.chat.consumer;
 
 import com.liaocyu.openChat.common.common.constant.MQConstant;
+import com.liaocyu.openChat.common.common.domain.dto.MsgSendMessageDTO;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.stereotype.Component;
 
 /**
@@ -9,8 +11,14 @@ import org.springframework.stereotype.Component;
  * @Project : openChat
  * @createTime : 2024/1/10 10:09
  * @description : 发送消息更新房间收信箱，并同步给房间成员信箱
+ * SEND_MSG_GROUP: chat_send_msg_group
+ * SEND_MSG_TOPIC: chat_send_msg
  */
-//@RocketMQMessageListener(consumerGroup = MQConstant.SEND_MSG_GROUP, topic = MQConstant.SEND_MSG_TOPIC)
+@RocketMQMessageListener(consumerGroup = MQConstant.SEND_MSG_GROUP, topic = MQConstant.SEND_MSG_TOPIC)
 @Component
-public class MsgSendConsumer {
+public class MsgSendConsumer implements RocketMQListener<MsgSendMessageDTO> {
+    @Override
+    public void onMessage(MsgSendMessageDTO msgSendMessageDTO) {
+
+    }
 }
